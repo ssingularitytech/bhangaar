548732b1f45d3f2c83e0591382d304f1d2013fe3
Sunny <sunny.220990@gmail.com>
2023-01-19 11:22:06 +0530

    New theme added

---
+++ app/controllers/landings_controller.rb
@@ -0,0 +1,5 @@
+class LandingsController < ApplicationController
+  # GET /landings or /landings.json
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :landings
   mount RailsAdmin::Engine => '/superadmin', as: 'rails_admin'
   devise_for :users
   namespace :admin do
